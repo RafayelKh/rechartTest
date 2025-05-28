ab4b7ddeef826e0c88aa1682042d6a26ed22716b
RafayelKh <dev.rafayelkh@gmail.com>
2025-05-28 10:52:01 +0400

    [add] dots colors

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -1,7 +1,8 @@
-import { CartesianGrid, LineChart, ResponsiveContainer, Line, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import { CartesianGrid, LineChart, ResponsiveContainer, Line, Legend, Tooltip, XAxis, YAxis, Dot } from "recharts";
 
-import { calcAverage, calcStandardDeviation, calcZScore, getGradientOffset } from "../util";
+import { calcAverage, calcStandardDeviation, calcZScore, convertBetweenAxes, getGradientOffset, normalize } from "../util";
 import { useMemo } from "react";
+import type { Props as DotProps } from "recharts/types/shape/Dot";
 
 export interface IChartBar {
   name: string;
@@ -21,6 +22,17 @@ const data = [
   { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
 ];
 
+export interface ICustomDotProps extends DotProps {
+  zScoreNormalized: number;
+  value: number;
+  min: number;
+  max: number;
+}
+
+const customDot = ({ zScoreNormalized, min, max, ...props}: ICustomDotProps) => {
+  const normalizedDotPosition = normalize(props.value, min, max);
+  return <Dot cx={props.cx} cy={props.cy} r={8} fill={normalizedDotPosition > zScoreNormalized ? 'red' : props.fill} stroke={normalizedDotPosition > zScoreNormalized ? 'red' : props.stroke} />
+}
 
 export const Chart = () => {
   const mean = useMemo(() => calcAverage(data.map(d => d.uv)), [data]);
@@ -31,8 +43,23 @@ export const Chart = () => {
     pvZ: calcZScore(d.pv, mean, standardDeviation),
   })), [data, mean, standardDeviation]);
 
-  const off1 = useMemo(() => getGradientOffset(enrichedData, 'uv', 'uvZ'), [enrichedData]);
-  const off2 = useMemo(() => getGradientOffset(enrichedData, 'pv', 'pvZ'), [enrichedData]);
+  const minUvZScore = useMemo(() => Math.min(...enrichedData.map(d => d.uvZ)), [enrichedData]);
+  const maxUvZScore = useMemo(() => Math.max(...enrichedData.map(d => d.uvZ)), [enrichedData]);
+  const minPvZScore = useMemo(() => Math.min(...enrichedData.map(d => d.pvZ)), [enrichedData]);
+  const maxPvZScore = useMemo(() => Math.max(...enrichedData.map(d => d.pvZ)), [enrichedData]);
+
+  const min = useMemo(() => Math.min(...enrichedData.map(d => d.uv), ...enrichedData.map(d => d.pv)), [enrichedData]);
+  const max = useMemo(() => Math.max(...enrichedData.map(d => d.uv), ...enrichedData.map(d => d.pv)), [enrichedData]);
+
+  // console.log(minUvZScore, maxUvZScore, minP vZScore, maxPvZScore);
+
+  const uvZScoreLevel = useMemo(() => convertBetweenAxes(1, [minUvZScore, maxUvZScore], [min, max]), []);
+  const pvZScoreLevel = useMemo(() => convertBetweenAxes(1, [minPvZScore, maxPvZScore], [min, max]), []);
+
+  const off1 = useMemo(() => 1 - normalize(uvZScoreLevel, min, max), [enrichedData]);
+  const off2 = useMemo(() => 1 - normalize(pvZScoreLevel, min, max), [enrichedData]);
+
+  // console.log(uvZScoreLevel, off1);
 
   return (
     <>
@@ -52,11 +79,11 @@ export const Chart = () => {
           <XAxis dataKey="name" />
           <YAxis yAxisId="left" domain={['dataMin', 'dataMax']} scale={'linear'} />
           <YAxis yAxisId="right" domain={['dataMin', 'dataMax']} scale={'linear'} orientation="right" />
-          <Tooltip />
+          <Tooltip contentStyle={{ backgroundColor: 'black', fontWeight: 'bold' }} />
           <Legend />
-          <Line type="monotone" dataKey="uv" stroke="url(#splitColor)" dot={{ fill: "#8884d8" }} activeDot={{ r: 8, fill: "#8884d8" }} yAxisId="left" />
-          <Line type="monotone" dataKey="pv" stroke="url(#splitColor2)" dot={{ fill: "#82ca9d" }} yAxisId="left" />
-          {/* <Line type="monotone" dataKey="uvZ" stroke="" dot={{ fill: "#82ca9d" }} yAxisId="right" /> */}
+          <Line type="monotone" dataKey="uv" stroke="url(#splitColor)" strokeWidth={3} dot={(e) => customDot({ ...e, zScoreNormalized: off1, min, max })} yAxisId="left" />
+          <Line type="monotone" dataKey="pv" stroke="url(#splitColor2)" strokeWidth={3} dot={(e) => customDot({ ...e, zScoreNormalized: off1, min, max })} yAxisId="left" />
+          {/* <Line type="monotone" dataKey="uvZ" stroke="green" dot={{ fill: "#82ca9d" }} yAxisId="right" /> */}
           <defs>
             <linearGradient id="splitColor" x1="0%" y1="0%" x2="0%" y2="100%">
               <stop offset={off1} stopColor="#ff0000" />
@@ -65,7 +92,7 @@ export const Chart = () => {
 
             <linearGradient id="splitColor2" x1="0%" y1="0%" x2="0%" y2="100%">
               <stop offset={off2} stopColor="#ff0000" />
-              <stop offset={off2} stopColor="#0000ff" />
+              <stop offset={off2} stopColor="#00ffff" />
             </linearGradient>
           </defs>
         </LineChart>
