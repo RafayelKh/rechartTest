16c1edb543d5f4562f8518983756c09b1ae4a25b
RafayelKh <dev.rafayelkh@gmail.com>
2025-05-28 09:55:06 +0400

    [add] z-score level on graph

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -1,6 +1,6 @@
 import { CartesianGrid, LineChart, ResponsiveContainer, Line, Legend, Tooltip, XAxis, YAxis } from "recharts";
 
-import { calcAverage, calcStandardDeviation, calcZScore } from "../util";
+import { calcAverage, calcStandardDeviation, calcZScore, getGradientOffset } from "../util";
 import { useMemo } from "react";
 
 export interface IChartBar {
@@ -8,6 +8,8 @@ export interface IChartBar {
   uv: number;
   pv: number;
   amt: number;
+  uvZ: number;
+  pvZ: number;
 }
 
 const data = [
@@ -20,41 +22,54 @@ const data = [
 ];
 
 
-
 export const Chart = () => {
   const mean = useMemo(() => calcAverage(data.map(d => d.uv)), [data]);
   const standardDeviation = useMemo(() => calcStandardDeviation(data.map(d => d.uv)), [data]);
-  const dataWithZScores: IChartBar[] = useMemo(() => data.map(d => ({
+  const enrichedData: IChartBar[] = useMemo(() => data.map(d => ({
     ...d,
     uvZ: calcZScore(d.uv, mean, standardDeviation),
     pvZ: calcZScore(d.pv, mean, standardDeviation),
   })), [data, mean, standardDeviation]);
 
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart
-        width={500}
-        height={300}
-        data={dataWithZScores}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
+  const off1 = useMemo(() => getGradientOffset(enrichedData, 'uv', 'uvZ'), [enrichedData]);
+  const off2 = useMemo(() => getGradientOffset(enrichedData, 'pv', 'pvZ'), [enrichedData]);
 
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis yAxisId="left" />
-        <YAxis yAxisId="right" orientation="right" />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" fill="url(#splitColor)" dot={{ fill: "#8884d8" }} activeDot={{ r: 8, fill: "#8884d8" }} yAxisId="left" />
-        <Line type="monotone" dataKey="uv"  fill="url(#splitColor)" dot={{ fill: "#82ca9d" }} yAxisId="left" />
-        <Line type="monotone" dataKey="pvZ" stroke="#ff7300" dot={{ fill: "#ff7300" }} name="pv z-score" yAxisId="right" />
-        <Line type="monotone" dataKey="uvZ" stroke="#00ff79" dot={{ fill: "#00ff79" }} name="uv z-score" yAxisId="right" />
-      </LineChart>
-    </ResponsiveContainer>
+  return (
+    <>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={enrichedData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis yAxisId="left" domain={['dataMin', 'dataMax']} scale={'linear'} />
+          <YAxis yAxisId="right" domain={['dataMin', 'dataMax']} scale={'linear'} orientation="right" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="uv" stroke="url(#splitColor)" dot={{ fill: "#8884d8" }} activeDot={{ r: 8, fill: "#8884d8" }} yAxisId="left" />
+          <Line type="monotone" dataKey="pv" stroke="url(#splitColor2)" dot={{ fill: "#82ca9d" }} yAxisId="left" />
+          {/* <Line type="monotone" dataKey="uvZ" stroke="" dot={{ fill: "#82ca9d" }} yAxisId="right" /> */}
+          <defs>
+            <linearGradient id="splitColor" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset={off1} stopColor="#ff0000" />
+              <stop offset={off1} stopColor="#0000ff" />
+            </linearGradient>
+
+            <linearGradient id="splitColor2" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset={off2} stopColor="#ff0000" />
+              <stop offset={off2} stopColor="#0000ff" />
+            </linearGradient>
+          </defs>
+        </LineChart>
+      </ResponsiveContainer>
+    </>
   );
 };
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,9 @@
+import type { IChartBar } from "./Chart/Chart";
+
+
+type IChartBarKey = keyof Omit<IChartBar, 'name'>;
+
+
 export const calcAverage = (data: number[]): number => {
   return data.reduce((sum, x) => sum + x, 0) / data.length;
 }
@@ -8,6 +14,30 @@ export const calcStandardDeviation = (data: number[]): number => {
   return Math.sqrt(variance);
 }
 
-export function calcZScore(value: number, average: number, standardDeviation: number): number {
+export const calcZScore = (value: number, average: number, standardDeviation: number): number => {
   return (value - average) / standardDeviation;
-} 
+}
+
+export const convertBetweenAxes = (value: number, sourceAxis: [number, number], targetAxis: [number, number]): number => {
+  const [sourceMin, sourceMax] = sourceAxis;
+  const [targetMin, targetMax] = targetAxis;
+  
+  const relativePosition = normalize(value, sourceMin, sourceMax);
+  const targetValue = targetMin + (relativePosition * (targetMax - targetMin));
+  
+  return targetValue;
+}
+
+export const normalize = (value: number, min: number, max: number): number => {
+  return (value - min) / (max - min);
+}
+
+export const getGradientOffset = (dataWithZScores: IChartBar[], key: IChartBarKey, zKey: IChartBarKey) => {
+  const min = Math.min(...dataWithZScores.map(d => d[key]));
+  const max = Math.max(...dataWithZScores.map(d => d[key]));
+
+  const minZScore = Math.min(...dataWithZScores.map(d => d[zKey]));
+  const maxZScore = Math.max(...dataWithZScores.map(d => d[zKey]));
+
+  return 1 - normalize(convertBetweenAxes(1, [minZScore, maxZScore], [min, max]), min, max);
+}
